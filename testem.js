4fa387e01f524643ff0a2d469b1454bf217fefff
Petr Kosikhin <petr@hanzo.es>
2018-02-07 05:07:31 +0300

    disable sandbox

--- testem.js
+++ testem.js
@@ -16,7 +16,7 @@ module.exports = {
         '--headless',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
-        '--disable-setuid-sandbox'
+        '--no-sandbox'
       ]
     }
   }
